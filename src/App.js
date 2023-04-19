3f4362c072304e017a3b99aa2c491b69f4d54e0f
Valentin <valio990210@gmail.com>
2023-04-19 19:49:57 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ function App() {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': 'd784bbf8bemsh937dbecae345d21p133f8ejsne9677a6edd2d',
-      'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+      'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
     }
   };
   
-  fetch('https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt0001702%2Ctt0001856%2Ctt0001856', options)
+  fetch('https://edamam-food-and-grocery-database.p.rapidapi.com/api/food-database/v2/parser?nutrition-type=cooking&category%5B0%5D=generic-foods&health%5B0%5D=alcohol-free', options)
     .then(response => response.json())
+    .then(data => console.log(data.hints))
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+
+    const [query, setQuery] = useState('')
+    const [container, ueContaier] = useState([])
+
+
   
   return (
-
-
 
     <div className="App">
 
